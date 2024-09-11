07d302c87aa5d1db17f634461b14470a6cf97d71
Jonathan Easterman <jonathaneasterman@gmail.com>
2024-09-11 14:23:20 -0700

    API changes for lagrange zk coprocessor v1

--- contracts/interfaces/lagrange/ILPNClientV1.sol
+++ contracts/interfaces/lagrange/ILPNClientV1.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {QueryOutput} from "./ILPNRegistryV1.sol";
+
 /**
  * @title ILPNClient
  * @notice Interface for the LPNClientV0 contract.
  */
-interface ILPNClient {
+interface ILPNClientV1 {
     /// @notice Callback function called by the LPNRegistry contract.
     /// @param requestId The ID of the request.
-    /// @param results The result of the request.
-    function lpnCallback(uint256 requestId, uint256[] calldata results)
-        external;
+    /// @param result The result of the request.
+    function lpnCallback(uint256 requestId, QueryOutput calldata result) external;
 }
